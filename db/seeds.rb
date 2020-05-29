b6c8a1dedc94af86e1c49796be83c9ad3d849fb1
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-05-29 14:55:40 -0700

    Use Faker with a loop to seed database. Seed 50 products and 250 reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+Review.destroy_all
+
+50.times do |index|
+  @product = Product.create!(name: Faker::Food.ingredient, 
+  country_of_origin: Faker::Address.country, 
+  cost: Faker::Number.decimal(l_digits: 2, r_digits: 2))
+
+  5.times do |index|
+    @reviews = @product.reviews.create!(author: Faker::FunnyName.name,
+    content_body: Faker::Lorem.paragraph_by_chars(number: 150),
+    rating: Faker::Number.within(range: 1..5))
+  end
+end
+
+p "Created #{Product.count} products"
+p "Created #{Review.count} reviews"
