d70b5db6a9e6112677255a103a40be45714cef2f
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-05-29 11:26:00 -0700

    Create validations to ensure that all fields should be filled out

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :country_of_origin, presence: true
+  validates :cost, presence: true
 end
