5ccaafd9b600f984da99f93e600e9dac9ae21a3f
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-06-05 09:20:24 -0700

    Add User Model and add methods to the User Model. Add routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_29_161753) do
+ActiveRecord::Schema.define(version: 2020_06_05_161323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 2020_05_29_161753) do
     t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_hash"
+    t.string "password_salt"
   end
 
   add_foreign_key "reviews", "products"
